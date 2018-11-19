f5d86d6ad238b9e0b9bf5e6e65b253c876885e22
VaniushaR <vaniusha.ra@gmail.com>
2018-11-19 13:51:23 -0600

    Updates

---
+++ precache-manifest.fa6700cb153dfe740003b9a777a4a133.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dfc8708f50a5f341417caaf7130ee15",
+    "url": "/static/media/Vania2.6dfc8708.jpg"
+  },
+  {
+    "revision": "6eaa22ac3e293d0a176ffd09181d0c78",
+    "url": "/static/media/LogoV.6eaa22ac.png"
+  },
+  {
+    "revision": "57df8bfe618a32f327ea70f29835cfaa",
+    "url": "/static/media/Español CV.57df8bfe.pdf"
+  },
+  {
+    "revision": "75431b5408c15570cfbdc65c01d73f2f",
+    "url": "/static/media/English CV.75431b54.pdf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f5791dc73ef71bc2d71",
+    "url": "/static/js/main.0f5791dc.chunk.js"
+  },
+  {
+    "revision": "b03ea9887c07b1b1ddc2",
+    "url": "/static/js/1.b03ea988.chunk.js"
+  },
+  {
+    "revision": "0f5791dc73ef71bc2d71",
+    "url": "/static/css/main.443476d4.chunk.css"
+  },
+  {
+    "revision": "72d62fd2e28fd544f4cc188ab411448a",
+    "url": "/index.html"
+  }
+];
